32deb3a62cfb0a63f32d6c586c1ced0774d44228
zuhdil <zuhdil@gmail.com>
2019-07-02 00:28:31 +0700

    Laravel package boilerplate.

---
+++ readme.md
@@ -0,0 +1,14 @@
+# pmi-donatur
+
+## Install
+`composer require bajaklautmalaka/pmi-donatur`
+
+## Usage
+Write a few lines about the usage of this package.
+
+## Testing
+Run the tests with:
+
+``` bash
+vendor/bin/phpunit
+```
---
+++ src/PmiDonaturServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace BajakLautMalaka\PmiDonatur;
+
+use Illuminate\Support\ServiceProvider;
+
+class PmiDonaturServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
