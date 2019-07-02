ee5dbbb723fa688eaa5f0611e6b68bb6a0f627fa
Aquino Tegar <aquinotgr@pm.me>
2019-07-02 22:04:12 +0700

    load config and migrations from donatorprovider

--- src/PmiDonaturServiceProvider.php
+++ src/PmiDonaturServiceProvider.php
@@ -3,6 +3,7 @@
 namespace BajakLautMalaka\PmiDonatur;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Database\Eloquent\Factory;
 
 class PmiDonaturServiceProvider extends ServiceProvider
 {
@@ -21,8 +22,65 @@ class PmiDonaturServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(Factory $factory)
     {
-        //
+        $this->mergeAuthConfig();
+        $this->loadConfig();
+        $this->loadMigrationsAndFactories($factory);
+    }
+
+    /**
+     * Merger any auth config from package donator.
+     *
+     * @return void
+     */
+    private function mergeAuthConfig()
+    {
+        /** @var \Illuminate\Config\Repository */
+        $config = $this->app['config'];
+
+        $original = $config->get('auth', []);
+        $toMerge = require __DIR__ . '/../config/auth.php';
+
+        $auth = [];
+        foreach ($original as $key => $value) {
+            $auth[$key] = $value;
+            if (isset($toMerge[$key])) {
+                $auth[$key] = array_merge($value, $toMerge[$key]);
+            }
+        }
+
+        $config->set('auth', $auth);
+    }
+
+    /**
+     * Register any load config.
+     *
+     * @return void
+     */
+    private function loadConfig()
+    {
+        $path = __DIR__.'/../config/donator.php';
+        $this->mergeConfigFrom($path, 'donator');
+
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                $path => config_path('donator.php'),
+            ], 'donator:config');
+        }
+    }
+
+    /**
+     * Register any load migrations & factories from package donators.
+     *
+     * @return void
+     */
+    private function loadMigrationsAndFactories(Factory $factory): void
+    {
+        if ($this->app->runningInConsole()) {
+            $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+
+            $factory->load(__DIR__.'/../database/factories');
+        }
     }
 }
