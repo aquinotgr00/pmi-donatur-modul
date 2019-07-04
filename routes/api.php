678a1c612ae4d77d3606a32cb775679c4f026350
Aquino Tegar <aquinotgr@pm.me>
2019-07-04 11:13:37 +0700

    grouping the donators api routes

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,14 @@
 <?php
 
-// API ROUTES 
-Route::post('signin', 'DonatorController@signin')->name("auth.donator.signin");
-Route::post('signup', 'DonatorController@signup')->name("auth.donator.signup");
-Route::post('token/verify', 'DonatorController@verifyDonatorAccessToken')->name("auth.donator.token.verify");
-Route::post('token/signin', 'DonatorController@tokenMemberVerification')->name("auth.donator.token.signin");
-Route::post('password/reset', 'DonatorController@createTokenForgotPassword')->name("auth.donator.token.password.reset");
-Route::post('password/change', 'DonatorController@changePassword')->name("auth.donator.token.password.change");
+// API DONATOR ROUTES 
+Route::group(['prefix' => 'donators'], function () {
+    Route::post('signin', 'DonatorController@signin')->name("auth.donator.signin");
+    Route::post('signup', 'DonatorController@signup')->name("auth.donator.signup");
+    Route::post('token/verify', 'DonatorController@verifyDonatorAccessToken')->name("auth.donator.token.verify");
+    Route::post('token/signin', 'DonatorController@tokenMemberVerification')->name("auth.donator.token.signin");
+    Route::post('password/reset', 'DonatorController@createTokenForgotPassword')->name("auth.donator.token.password.reset");
+    Route::post('password/change', 'DonatorController@changePassword')->name("auth.donator.token.password.change");
+});
 
 
 Route::get('campaigns', 'CampaignApiController@index')->name("campaigns.index");
