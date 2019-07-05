9ac5ac2384d98ff2acf2db87b058a0271ee5509c
Aquino Tegar <aquinotgr@pm.me>
2019-07-05 17:33:48 +0700

    add donator signin, signup, forgot password and update profile

---
+++ src/PasswordReset.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace BajakLautMalaka\PmiDonatur;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PasswordReset extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['token', 'email'];
+}
--- src/controllers/Api/DonatorController.php
+++ src/controllers/Api/DonatorController.php
@@ -6,6 +6,7 @@ use Illuminate\Routing\Controller;
 use Illuminate\Http\Request;
 
 use BajakLautMalaka\PmiDonatur\Donator;
+use BajakLautMalaka\PmiDonatur\PasswordReset;
 
 use Illuminate\Support\Carbon;
 
@@ -22,6 +23,13 @@ class DonatorController extends Controller
      * @var mixed donators
      */
     protected $donators;
+
+    /**
+     * Create a new parameter.
+     *
+     * @var mixed passwordResets
+     */
+    protected $passwordResets;
     
     /**
      * Create a new parameter.
@@ -44,10 +52,12 @@ class DonatorController extends Controller
      */
     public function __construct(
         Donator $donatorModel,
+        PasswordReset $passwordResets,
         User    $users
     )
     {
         $this->donators = $donatorModel;
+        $this->passwordResets = $passwordResets;
         $this->users    = $users;
     }
 
@@ -67,6 +77,11 @@ class DonatorController extends Controller
         // TODO: add custom user fields to config so that anyone could adjust
         $user = $this->users->create($request->only(['name', 'email', 'password']));
         $token = $user->createToken('Personal Access Token');
+        
+        // add user id to request
+        $request->request->add([
+            'user_id' => $user->id
+        ]);
 
         // create donator
         $this->donators->create($request->except(['email', 'password', 'url_action']));
@@ -94,7 +109,7 @@ class DonatorController extends Controller
      *
      * @param BajakLautMalaka\PmiDonatur\Requests\SigninPostRequest $request
      * 
-     * @return mixed|boolean
+     * @return mixed
      */
     public function signin(SigninPostRequest $request)
     {
@@ -119,124 +134,6 @@ class DonatorController extends Controller
 
         return response()->json($response, 200);
     }
-    
-    /**
-     * Access token login donator verification to verified donator
-     *
-     * @param  string|null $token
-     * @var boolean $access
-     * @param  \Illuminate\Http\Request  $request
-     *
-     * @return mixed|boolean $access
-     * @return mixed|boolean
-     */
-    public function tokenMemberVerification(Request $request)
-    {
-        $request->validate([
-            'token' => 'required|string'
-        ]);
-
-        $access = $this->access_token->verifyToken($request->token);
-
-        $response = ["message" => "Token does not exist."];
-        $status = 422;
-
-        if (!is_null($access)) {
-            $user = $this->donators->find($access->donator_id);
-
-            if (!is_null($user)) {
-                $tokenResult = $user->createToken('Member Grant Client');
-                $token = $tokenResult->token;
-                $token->expires_at = Carbon::now()->addWeeks(1);
-                $token->save();
-
-                $response = [
-                    'access_token' => $tokenResult->accessToken,
-                    'token_type' => 'Bearer',
-                    'expires_at' => Carbon::parse(
-                                        $tokenResult->token->expires_at
-                                    )->toDateTimeString()
-                ];
-                $status = 201;
-            }
-        }
-
-        return response()->json($response, $status);
-    }
-
-    /**
-     * Access token login donator to get oauth Client
-     *
-     * @param  string $data
-     *
-     * @return string $access_token
-     */
-    public function accessTokenDonator($donatorId)
-    {
-        $token = sha1(Carbon::now()->timestamp."".$donatorId);
-        $access_token = $this->access_token->create([
-            'donator_id' => $donatorId,
-            'token' => $token,
-            'expired_at' => Carbon::now()->addWeeks(1)
-        ]);
-        return $access_token->token;
-    }
-
-     /**
-     * Access token login donator verification to verified donator
-     *
-     * @param  string|null $token
-     * @var boolean $access
-     * @param  \Illuminate\Http\Request  $request
-     *
-     * @return mixed|boolean $access
-     * @return mixed|boolean
-     */
-    public function verifyDonatorAccessToken(Request $request)
-    {
-        $request->validate([
-            'token' => 'required|string'
-        ]);
-
-        $accessToken = $this->access_token->verifyToken($request->token);
-
-        $response = ["message" => "Token does not exist."];
-        $status = 422;
-
-        if (!is_null($accessToken)) {
-            $donator = $this->donators->find($accessToken->donator_id);
-
-            if (!is_null($donator)) {
-                $tokenResult = $donator->createToken('Member Grant Client');
-                $token = $tokenResult->token;
-                $token->expires_at = Carbon::now()->addWeeks(1);
-                $token->save();
-
-                $response = [
-                    'access_token' => $tokenResult->accessToken,
-                    'token_type' => 'Bearer',
-                    'expires_at' => Carbon::parse(
-                        $tokenResult->token->expires_at
-                    )->toDateTimeString()
-                ];
-                $status = 201;
-            }
-        }
-        
-        return response()->json($response, $status);
-    }
-
-    /**
-     * sending email and access token to Forgot / reset password
-     *
-     * @param  array  $data
-     *
-     * @return void
-     */
-    public function sendEmailAndTokenReset($data)
-    {
-        dispatch(new \BajakLautMalaka\PmiDonatur\Jobs\SendEmailDonatorResetPassword($data));
-    }
 
     /**
      * requets to generate token for forgot password member using api and generate email
@@ -250,31 +147,91 @@ class DonatorController extends Controller
     {
         $request->validate([
             'email' => 'required|string|email',
+            'url_action' => 'required'
         ]);
 
         $response = [
-            'message'=>'Email not found'
+            'message' => 'Email not found'
         ];
         $status = 404;
 
-        $donator = $this->donators->findForPassport($request->email);
+        $donator = $this->users->where('email', $request->email)->first();
         if (!is_null($donator)) {
             $token = sha1(Carbon::now()->timestamp."".$donator->id);
-            $this->password_reset->create(['token' => $token, 'email' => $request->email]);
+            $this->passwordResets->create(['token' => $token, 'email' => $request->email]);
+
+            $data = [
+                'email' => $request->email,
+                'content' => $request->url_action."/".$token
+            ];
+            $this->donators->sendEmailAndTokenReset($data);
 
             $response = [
-                'access_token' => $token
+                'reset_password_token' => $token
             ];
-            if (!is_null($request->url_act)) {
-                $data=[
-                    'email' => $request->email,
-                    'content' => $request->url_act."/".$token
-                ];
-                $this->sendEmailAndTokenReset($data);
-            }
             $status = 200;
         }
         
+        return response()->json($response, $status);
+    }
+
+    /**
+     * Change password using verified token
+     *
+     * @param   \Illuminate\Http\Request  $request
+     * 
+     * @return mixed
+     *
+     */
+    public function changePassword(Request $request)
+    {
+        $request->validate([
+            "token" => "required",
+            "password" => "required|confirmed"
+        ]);
+
+        $response = ["message" => "Email / token not valid"];
+        $status = 400;
+
+        $check = $this->passwordResets->where('token', $request->token)->first();
+        if (!is_null($check)) {
+            $user = $this->users->where('email', $check->email)->first();
+            $this->users->where('email', $check->email)->update(['password' => bcrypt($request->password)]);
+            $token = $user->createToken('New Personal Access Token');
+            
+            $data = [
+                'email' => $check->email,
+                'content' => 'Password kamu berhasil di ubah.'
+            ];
+            $this->donators->sendEmailSuccess($data);
+
+            $response = [
+                "message" => "Password sucessfully changed.",
+                "access_token" => $token->accessToken
+            ];
+            $status = 200;
+        }
+        return response()->json($response, $status);
+    }
+
+    /**
+     * Edit the donator data
+     *
+     * @param Request $request
+     * @param integer $id
+     * @return mixed
+     */
+    public function updateDonatorProfile(Request $request, $id)
+    {
+        $this->guard('auth:api');
+        $donator = $this->donators->find($id);
+        $donator->update($request->all());
+        
+        $response = [
+            "message" => "Your data sucessfully changed.",
+        ];
+        $status = 200;
+
         return response()->json($response, $status);
     }
 }
