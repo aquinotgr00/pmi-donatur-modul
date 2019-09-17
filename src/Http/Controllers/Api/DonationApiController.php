7f9fa978ef573b12a56d2153d2b7211551262c9c
ifirmawan <firmawaneiwan@gmail.com>
2019-09-17 22:03:16 +0700

    save donator before create a donation

--- src/Http/Controllers/Api/DonationApiController.php
+++ src/Http/Controllers/Api/DonationApiController.php
@@ -106,6 +106,14 @@ class DonationApiController extends Controller
                 'status' => 3
             ]);
         }
+        //save donator name at list donator
+        if ($request->has('phone')) {
+            $donatorData    = $request->only('name','phone');
+            $donator        = Donator::firstOrCreate($donatorData);
+            $request->merge([
+                'donator_id' => $donator->id
+            ]);
+        }
 
         $this->makeInvoiceID($request);
             
@@ -216,17 +224,8 @@ class DonationApiController extends Controller
         }
 
         if ($request->has('phone')) {
-            
-            $donator = Donator::firstOrCreate(
-                [
-                    'phone'=> $request->phone 
-                ],
-                [
-                    'name' => $request->name,
-                    'email' => $request->email,
-                    'phone' => $request->phone
-                ]
-            );
+            $donatorData = $request->only('name','phone');
+            $donator = Donator::firstOrCreate($donatorData);
 
             if ($request->has('address')) {
                 
